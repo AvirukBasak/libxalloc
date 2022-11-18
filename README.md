2ecfebb2e19ae4d30b5bfb7541c61934d3bb2878
AvirukBasak <avirukbasak@gmail.com>
2022-11-18 21:14:13 +0530

    added basic reallocator

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Custom heap memory allocator using `sbrk` and `mmap`.
 - Visit [test.c](tests/test.c) for example use.
 
 ## Warning
-Writing to invalid memory allocated by `liballoc` will break the memory cleanup function.
+- Writing to invalid memory allocated by `liballoc` will break the memory cleanup function.
+- segmentation faults during execution can be attributed to `libc`.
+- `libc` will break functionality of library.
--- src/alloc.c
+++ src/alloc.c
@@ -42,19 +42,32 @@ struct ALLOC_membloc_st
     ALLOC_membloc_t *nxt;
 };
 
+#define ALLOC_ALLOCRE_COPY_THRESHOLD (4096)
+
 /** linked list of block data */
 ALLOC_mhead_t *ALLOC_memhead = NULL;
 
 /** searches for a specific block data based on its address */
 ALLOC_membloc_t *ALLOC_mblock_find(void *ptr)
 {
-    typedef ALLOC_membloc_t *node;
-    node p = ALLOC_memhead->start;
+    typedef ALLOC_membloc_t *node_t;
+    node_t p = ALLOC_memhead->start;
     while (p) {
         if (p->ptr == ptr) return p;
         p = p->nxt;
     }
     return NULL;
+}
+
+void ALLOC_allocate_head()
+{
+    if (!ALLOC_memhead) {
+        ALLOC_memhead = sbrk(sizeof(ALLOC_mhead_t));
+        ALLOC_NULLCHECK(ALLOC_memhead);
+        ALLOC_memhead->blockc = 0;
+        ALLOC_memhead->start = NULL;
+        ALLOC_memhead->end = NULL;
+    }
 }
 
 ALLOC_membloc_t *ALLOC_allocate_m(size_t size, void *ptr)
@@ -86,13 +99,32 @@ void ALLOC_linkup(ALLOC_membloc_t *node)
 /** alloctes specified size */
 void *allocm(size_t size)
 {
-    if (!ALLOC_memhead) {
-        ALLOC_memhead = sbrk(sizeof(ALLOC_mhead_t));
-        ALLOC_NULLCHECK(ALLOC_memhead);
-        ALLOC_memhead->blockc = 0;
-        ALLOC_memhead->start = NULL;
-        ALLOC_memhead->end = NULL;
+    if (!ALLOC_memhead)
+        ALLOC_allocate_head();
+
+    typedef ALLOC_membloc_t *node_t;
+
+    // attempting to recycle old empty block
+    if (ALLOC_memhead->start) {
+        node_t reusable = ALLOC_memhead->start;
+        while (reusable)
+            if (reusable->size >= size) break;
+            else reusable = reusable->nxt;
+        if (reusable) {
+            reusable->free = false;
+            if (reusable->size == size) return reusable->ptr;
+            node_t leftover = (node_t) (reusable->ptr + size);
+            leftover->free = true;
+            leftover->ptr = (void *) (leftover + sizeof(ALLOC_membloc_t));
+            leftover->size = reusable->size - size;
+            leftover->prv = reusable;
+            leftover->nxt = reusable->nxt;
+            reusable->nxt = leftover;
+            return reusable->ptr;
+        }
     }
+
+    // allocating new block
     void *ptr = sbrk(size);
     ALLOC_linkup(ALLOC_allocate_m(size, ptr));
     return ptr;
@@ -101,6 +133,20 @@ void *allocm(size_t size)
 /** resizes allocated block if possible, or copies data around */
 void *allocre(void *ptr, size_t size)
 {
+    if (!ptr) allocm(size);
+    ALLOC_membloc_t *block = ALLOC_mblock_find(ptr);
+    ALLOC_NULLCHECK(block);
+    // if (block->size > ALLOC_ALLOCRE_COPY_THRESHOLD)
+        if (!block->nxt) {
+            sbrk(size - block->size);
+            block->size = size;
+            return ptr;
+        } else {
+            void *newptr = allocm(size);
+            memcpy(newptr, block->ptr, MIN(block->size, size));
+            block->free = true;
+            return newptr;
+        }
     return NULL;
 }
 
@@ -112,4 +158,13 @@ void alloc_free(void *ptr)
     ALLOC_membloc_t *block = ALLOC_mblock_find(ptr);
     ALLOC_NULLCHECK(block);
     block->free = true;
+    if (!block->nxt) {
+        if (block->prv) block->prv->nxt = NULL;
+        else {
+            ALLOC_memhead->start = NULL;
+            ALLOC_memhead->end = NULL;
+        }
+        ALLOC_memhead->blockc--;
+        brk(block);
+    }
 }

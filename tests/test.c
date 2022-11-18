1236ef7f1e10023610ad018f89517eff24801288
AvirukBasak <avirukbasak@gmail.com>
2022-11-18 19:18:54 +0530

    updated test

--- tests/test.c
+++ tests/test.c
@@ -4,11 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-    char *s = allocm(33);
-    for (int i = 0; i < 26; i++) {
-        s[i] = i + 0x61;
+    for (int i = 0; i < 7; i++) {
+        char *s = allocm(33);
+        for (int i = 0; i < 26; i++) {
+            s[i] = i + 0x61;
+        }
+        printf("%d: %s\n", i, s);
+        alloc_free(s);
     }
-    printf("%s\n", s);
-    alloc_free(s);
     return 0;
 }

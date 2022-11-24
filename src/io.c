30884de63bccc0617d6d61765d5f71973cc69d2d
AvirukBasak <avirukbasak@gmail.com>
2022-11-25 00:10:54 +0530

    updated pointer printing

--- src/io.c
+++ src/io.c
@@ -15,14 +15,24 @@ void _xalloc_print_ptr(int fd, const ptr_t p)
 {
     char *b = (char*)(&p);
     char s[16];
+    size_t len = 0;
+    bool msb_zero = true;
     bool is_bend = _xalloc_std_is_litle_endian();
     int i = is_bend ? 7 : 0;
-    for (int j = 0; (is_bend ? (i >= 0) : (i < 8)) && j < 16; j+=2) {
+    for (int j = 0; (is_bend ? (i >= 0) : (i < 8)) && j < 16;) {
         const ui8_t byte = b[i];
-        s[j] = _xalloc_std_to_hex(byte >> 4);
-        s[j+1] = _xalloc_std_to_hex(byte);
+        char halfbyte0 = _xalloc_std_to_hex(byte >> 4);
+        char halfbyte1 = _xalloc_std_to_hex(byte);
+        if (halfbyte0 != '0' && halfbyte1 != '0')
+            msb_zero = false;
+        if (!msb_zero) {
+            s[j] = halfbyte0;
+            s[j+1] = halfbyte1;
+            j+=2;
+            len++;
+        }
         is_bend ? i-- : i++;
     }
     write(fd, "0x", 2);
-    write(fd, s, 16);
+    write(fd, s, 2*len);
 }

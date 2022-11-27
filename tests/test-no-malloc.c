bdb8f8748cd40b2306e2727c3fc23d195e64cb82
AvirukBasak <avirukbasak@gmail.com>
2022-11-27 20:18:43 +0530

    tests/test-no-malloc.c: replaced xmalloc with malloc

--- tests/test-no-malloc.c
+++ tests/test-no-malloc.c
@@ -57,27 +57,27 @@ int main(int argc, char *argv[])
     void *p0, *p1;
     printf("brk init = %p\n", p0 = sbrk(0));
     for (int i = 0; i < 7; i++) {
-        char *s0 = xmalloc(SZ);
+        char *s0 = malloc(SZ);
         for (int i = 0; i < SZ; i++) {
             s0[i] = i%26 + 0x61;
         }
-        char *s1 = xmalloc(SZ);
+        char *s1 = malloc(SZ);
         for (int i = 0; i < SZ; i++) {
             s1[i] = i%26 + 0x61;
         }
-        char *s2 = xmalloc(SZ);
+        char *s2 = malloc(SZ);
         for (int i = 0; i < SZ; i++) {
             s2[i] = i%26 + 0x61;
         }
-        char *s3 = xmalloc(SZ);
+        char *s3 = malloc(SZ);
         for (int i = 0; i < SZ; i++) {
             s3[i] = i%26 + 0x61;
         }
         s3[26] = 0; printf("%d: %s\n", i, s3);
-        xfree(s0);
-        xfree(s1);
-        xfree(s2);
-        xfree(s3);
+        free(s0);
+        free(s1);
+        free(s2);
+        free(s3);
     }
     printf("brk exit = %p\n", p1 = sbrk(0));
     printf("brk difference = %zu B\n", (size_t) (p1 - p0));

ffee8ae8d115a388a541f7b7f99e0b4e1c25c2e0
AvirukBasak <avirukbasak@gmail.com>
2022-11-27 19:51:22 +0530

    added a test that uses no malloc
    overrding libc allocators with custom allocators

---
+++ tests/test-no-malloc.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "libxalloc.h"
+#define SZ (256*1024*1024)
+
+/* These functions comes from libxalloc. Since libxalloc
+ * doesn't provide the declarations of these functions, I've
+ * put the declarations here.
+ * These functions simply prints a string/number, but use Linux
+ * system calls directly and don't need to allocate memory, which
+ * prevent them from interfering with our memory allocation activities.
+ */
+void __xalloc_print_str(int fd, const char *s);
+void __xalloc_print_ui64(int fd, size_t size);
+
+/* overriding libc allocators with custom functions.
+ * note that if printf calls a heap allocator, it'll
+ * call one of the following
+ */
+
+void *malloc(size_t size)
+{
+    __xalloc_print_str(2, ">> malloc called with size = '");
+    __xalloc_print_ui64(2, size);
+    __xalloc_print_str(2, " B'\n");
+    return xmalloc(size);
+}
+
+void *calloc(size_t count, size_t size)
+{
+    __xalloc_print_str(2, ">> calloc called with size = '");
+    __xalloc_print_ui64(2, size);
+    __xalloc_print_str(2, " B'\n");
+    void *p = xmalloc(count * size);
+    memset(p, 0, count * size);
+    return p;
+}
+
+void *realloc(void *p, size_t size)
+{
+    __xalloc_print_str(2, ">> realloc called with size = '");
+    __xalloc_print_ui64(2, size);
+    __xalloc_print_str(2, " B'\n");
+    return xrealloc(p, size);
+}
+
+void free(void *p)
+{
+    __xalloc_print_str(2, ">> free called, freed = '");
+    __xalloc_print_ui64(2, xfree(p));
+    __xalloc_print_str(2, " B'\n");
+}
+
+int main(int argc, char *argv[])
+{
+    void *p0, *p1;
+    printf("brk init = %p\n", p0 = sbrk(0));
+    for (int i = 0; i < 7; i++) {
+        char *s0 = xmalloc(SZ);
+        for (int i = 0; i < SZ; i++) {
+            s0[i] = i%26 + 0x61;
+        }
+        char *s1 = xmalloc(SZ);
+        for (int i = 0; i < SZ; i++) {
+            s1[i] = i%26 + 0x61;
+        }
+        char *s2 = xmalloc(SZ);
+        for (int i = 0; i < SZ; i++) {
+            s2[i] = i%26 + 0x61;
+        }
+        char *s3 = xmalloc(SZ);
+        for (int i = 0; i < SZ; i++) {
+            s3[i] = i%26 + 0x61;
+        }
+        s3[26] = 0; printf("%d: %s\n", i, s3);
+        xfree(s0);
+        xfree(s1);
+        xfree(s2);
+        xfree(s3);
+    }
+    printf("brk exit = %p\n", p1 = sbrk(0));
+    printf("brk difference = %zu B\n", (size_t) (p1 - p0));
+    return 0;
+}
